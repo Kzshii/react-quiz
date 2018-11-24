cfbdc7cd7ba1cd852e78804d77aa00bb052df9b3
Kazushi Sakurada <kazushisakurada@icloud.com>
2018-11-24 08:02:56 -0800

    Updates

---
+++ precache-manifest.b22c5e9e11f6396a4fc4f7f128fc9568.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8b965a14a06d4c0906b5e55660a65c2",
+    "url": "/static/media/yoda.e8b965a1.jpeg"
+  },
+  {
+    "revision": "8f33a0cba074c566ccc22f352a91d7d8",
+    "url": "/static/media/luke.8f33a0cb.jpg"
+  },
+  {
+    "revision": "e361a0a49d74ac266be83b0688ff7b64",
+    "url": "/static/media/estrela.e361a0a4.jpeg"
+  },
+  {
+    "revision": "8355185d0c8679c921d1015e1eb2e985",
+    "url": "/static/media/darth.8355185d.jpeg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ba66c34d77f801a0ee4",
+    "url": "/static/js/main.7ba66c34.chunk.js"
+  },
+  {
+    "revision": "79439113bd7b30d084e2",
+    "url": "/static/js/1.79439113.chunk.js"
+  },
+  {
+    "revision": "7ba66c34d77f801a0ee4",
+    "url": "/static/css/main.54f7dee2.chunk.css"
+  },
+  {
+    "revision": "215ebd939119514ab5c6bd229633362b",
+    "url": "/index.html"
+  }
+];
